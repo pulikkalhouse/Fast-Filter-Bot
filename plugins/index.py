2f12cec2ec2290ddd59b1b75a8de02ce94a07b3d
Kichu <kichumon333@outlook.com>
2025-06-01 19:24:50 +0530

    Add files via upload

---
+++ plugins/index.py
@@ -0,0 +1,171 @@
+# (c) github - @Rishikesh-Sharma09 ,telegram - https://telegram.me/Rk_botz
+# removing credits doesn't make you coder 
+# New better way of indexing and skipping added 
+import logging
+import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.errors import FloodWait
+
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified, UserIsBlocked
+from info import ADMINS, LOG_CHANNEL, INDEX_EXTENSIONS
+from database.ia_filterdb import save_file
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+from utils import temp, get_readable_time
+import re, time
+
+lock = asyncio.Lock()
+
+@Client.on_callback_query(filters.regex(r'^index'))
+async def index_files(bot, query):
+    _, ident, chat, lst_msg_id, skip = query.data.split("#")
+    if ident == 'yes':
+        msg = query.message
+        await msg.edit("Starting Indexing...")
+        try:
+            chat = int(chat)
+        except:
+            chat = chat
+        await index_files_to_db(int(lst_msg_id), chat, msg, bot, int(skip))
+    elif ident == 'cancel':
+        temp.CANCEL = True
+        await query.message.edit("Trying to cancel Indexing...")
+
+@Client.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text ) & filters.private & filters.incoming)
+async def send_for_index(bot, message):
+    if message.text:
+        regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+        match = regex.match(message.text)
+        if not match:
+            return await message.reply('Invalid link')
+        chat_id = match.group(4)
+        last_msg_id = int(match.group(5))
+        if chat_id.isnumeric():
+            chat_id  = int(("-100" + chat_id))
+    elif message.forward_from_chat.type == enums.ChatType.CHANNEL:
+        last_msg_id = message.forward_from_message_id
+        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
+    else:
+        return
+    try:
+        await bot.get_chat(chat_id)
+    except ChannelInvalid:
+        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+    except (UsernameInvalid, UsernameNotModified):
+        return await message.reply('Invalid Link specified.')
+    except Exception as e:
+        logger.exception(e)
+        return await message.reply(f'Errors - {e}')
+    try:
+        k = await bot.get_messages(chat_id, last_msg_id)
+    except:
+        return await message.reply('Make Sure That Iam An Admin In The Channel, if channel is private')
+    if k.empty:
+        return await message.reply('This may be group and iam not a admin of the group.')
+
+    s = await message.reply_text(
+        text = "<b>Send the skip message number.\n\nIf dont want to skip any files send me 👉 0 \n</b>",
+        reply_to_message_id=message.id,
+        reply_markup=ForceReply(True)
+    )
+
+
+@Client.on_message(filters.private & filters.reply) 
+async def forceskip(client, message):      
+    reply_message = message.reply_to_message 
+    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):   
+        skip_msg = message
+        try:
+            skip = int(skip_msg.text)
+        except:
+            await message.reply("InValid Number provided using 0 as a skip number")
+            skip = 0
+        msg = await client.get_messages(message.chat.id, reply_message.id) 
+        info = msg.reply_to_message
+        if info.text:
+            regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+            match = regex.match(info.text)
+            if not match:
+                return await info.reply('Invalid link')
+            chat_id = match.group(4)
+            last_msg_id = int(match.group(5))
+            if chat_id.isnumeric():
+                chat_id  = int(("-100" + chat_id))
+        elif info.forward_from_chat.type == enums.ChatType.CHANNEL:
+            last_msg_id = info.forward_from_message_id
+            chat_id = info.forward_from_chat.username or info.forward_from_chat.id
+        else:
+            return
+
+    await message.delete()
+    await msg.delete()
+    if message.from_user.id in ADMINS:      
+        buttons = [
+            [
+                InlineKeyboardButton('Yes',
+                                     callback_data=f'index#yes#{chat_id}#{last_msg_id}#{skip}')
+            ],
+            [
+                InlineKeyboardButton('close', callback_data='close_data'),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        return await message.reply(
+            f'Do you Want To Index This Channel/ Group ?\n\nChat ID/ Username: <code>{chat_id}</code>\nLast Message ID: <code>{last_msg_id}</code>',
+            reply_markup=reply_markup)
+ 
+    
+    
+
+
+async def index_files_to_db(lst_msg_id, chat, msg, bot, skip):
+    start_time = time.time()
+    total_files = 0
+    duplicate = 0
+    errors = 0
+    deleted = 0
+    no_media = 0
+    unsupported = 0
+    current = skip
+    
+    async with lock:
+        try:
+            async for message in bot.iter_messages(chat, lst_msg_id, skip):
+                time_taken = get_readable_time(time.time()-start_time)
+                if temp.CANCEL:
+                    temp.CANCEL = False
+                    await msg.edit(f"Successfully Cancelled!\nCompleted in {time_taken}\n\nSaved <code>{total_files}</code> files to Database!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>\nUnsupported Media: <code>{unsupported}</code>\nErrors Occurred: <code>{errors}</code>")
+                    return
+                current += 1
+                if current % 250 == 0:
+                    btn = [[
+                        InlineKeyboardButton('CANCEL', callback_data=f'index#cancel#{chat}#{lst_msg_id}#{skip}')
+                    ]]
+                    await msg.edit_text(text=f"Total messages received: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>\nUnsupported Media: <code>{unsupported}</code>\nErrors Occurred: <code>{errors}</code>", reply_markup=InlineKeyboardMarkup(btn))
+                if message.empty:
+                    deleted += 1
+                    continue
+                elif not message.media:
+                    no_media += 1
+                    continue
+                elif message.media not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+                    unsupported += 1
+                    continue
+                media = getattr(message, message.media.value, None)
+                if not media:
+                    unsupported += 1
+                    continue
+                elif not (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
+                    unsupported += 1
+                    continue
+                media.caption = message.caption
+                sts = await save_file(message, media)
+                if sts == 'suc':
+                    total_files += 1
+                elif sts == 'dup':
+                    duplicate += 1
+                elif sts == 'err':
+                    errors += 1
+        except Exception as e:
+            await msg.reply(f'Index canceled due to Error - {e}')
+        else:
+            await msg.edit(f'Succesfully saved <code>{total_files}</code> to Database!\nCompleted in {time_taken}\n\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>\nUnsupported Media: <code>{unsupported}</code>\nErrors Occurred: <code>{errors}</code>')
