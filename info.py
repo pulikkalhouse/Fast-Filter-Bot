f99e4d3675b78cfaaa022991c810eca8fab08337
Kichu <kichumon333@outlook.com>
2025-06-02 13:16:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ if len(BOT_TOKEN) == 0:
 PORT = int(environ.get('PORT', '8080')) #don't change anything 
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/ryl.jpg')).split()
 
 # Bot Admins
 ADMINS = environ.get('ADMINS', '') #apni tg id daalo
