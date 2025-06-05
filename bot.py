8326a38bbb715b8c68017346ecef4556a60e4e51
Kichu <kichumon333@outlook.com>
2025-06-05 21:01:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 
 class Bot(Client):
