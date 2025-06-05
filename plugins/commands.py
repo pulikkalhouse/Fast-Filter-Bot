28e127da8d0f4066fbb9c863aa1be969b76a01df
Kichu <kichumon333@outlook.com>
2025-06-05 21:43:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,6 +13,7 @@ from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, de
 from database.users_chats_db import db
 from info import STICKERS_IDS, SUPPORT_GROUP, INDEX_CHANNELS, ADMINS, IS_VERIFY, VERIFY_TUTORIAL, VERIFY_EXPIRE, TUTORIAL, SHORTLINK_API, SHORTLINK_URL, AUTH_CHANNEL, DELETE_TIME, SUPPORT_LINK, UPDATES_LINK, LOG_CHANNEL, PICS, PROTECT_CONTENT, IS_STREAM, IS_FSUB, PAYMENT_QR, DATA_DATABASE_URL, FILES_DATABASE_URL, SECOND_FILES_DATABASE_URL
 from utils import get_settings, delayed_delete, get_size, is_subscribed, is_check_admin, get_shortlink, get_verify_status, update_verify_status, save_group_settings, temp, get_readable_time, get_wish, get_seconds
+from time import time as time_now
 
 @Client.on_message(filters.command("ask") & filters.incoming) #add your support grp to use this
 async def aiRes(_, message):
